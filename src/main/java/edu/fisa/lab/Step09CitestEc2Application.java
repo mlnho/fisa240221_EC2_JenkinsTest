1d76dc1a556a4400e9569f657378ba1f97c95e98
mlnho <1104minho@naver.com>
2024-02-21 14:47:30 +0900

    first commit for EC2 Test

---
+++ src/main/java/edu/fisa/lab/Step09CitestEc2Application.java
@@ -0,0 +1,13 @@
+package edu.fisa.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step09CitestEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step09CitestEc2Application.class, args);
+	}
+
+}
